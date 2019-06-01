e05c2e9cd9e912b71b6d14a22b17b5a087d5f74e
jaydray1 <jdreher15@gmail.com>
2019-06-01 05:07:04 -0600

    Background image additions, s3 additions, styling, sizing etc

--- src/app/retro-projects/retro-projects.component.ts
+++ src/app/retro-projects/retro-projects.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retro-projects.component.css']
 })
 export class RetroProjectsComponent implements OnInit {
-
+  browseImage = 'https://marmot-clone-start.s3-us-west-1.amazonaws.com/Screen+Shot+2019-06-01+at+4.25.18+AM.png';
+  
   constructor() { }
 
   ngOnInit() {
